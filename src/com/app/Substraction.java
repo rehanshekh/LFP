e7fe8c989feeebb736e7827a1517e95bb7ebeabc
Rehan Shekh <rehanrshekh@gmail.com>
2022-02-19 08:54:15 +0530

    All Programs

---
+++ src/com/app/Substraction.java
@@ -0,0 +1,9 @@
+package com.app;
+
+public class Substraction {
+    public static void main(String[] args) {
+        int x = 75, y = 10, sum;
+        sum=(x - y);
+        System.out.println("The value of substraction is:"+sum);
+    }
+}
