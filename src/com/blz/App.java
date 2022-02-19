de4af5a11ed4ae4eee05cd6749bae843d7333044
rehanshekh <98506415+rehanshekh@users.noreply.github.com>
2022-02-19 09:02:11 +0530

    Update App.java

--- src/com/blz/App.java
+++ src/com/blz/App.java
@@ -2,11 +2,7 @@ package com.blz;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello & Welcome, Friends");
-=======
-        System.out.println("Hello & Welcome, Ladies");
->>>>>>> 3651f642e857f2c93560ef44e84bd80a257e8b70
 
     }
 }
